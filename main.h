5cf2286898eecb8374a98f6c3b14f5b43ddd8d7d
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2023-11-20 04:38:20 -0800

    Printf function that hanldes %b format

--- main.h
+++ main.h
@@ -13,36 +13,6 @@ void print_binary(uint64_t num);
 
 
 /**
- * print_binary - Converts ints to binary and prints.
- *
- * @num: Int to be converted.
- * Return: nothing
- */
-
-void print_binary(uint64_t num)
-{
-	int j, i = 0;
-	int binary[64];
-
-	if (num == 0)
-	{
-	putchar('0');
-	return;
-	}
-
-	while (num > 0)
-	{
-		binary[i++] = num % 2;
-		num /= 2;
-	}
-
-	for (j = i - 1; j >= 0; j--)
-	{
-		putchar(binary[j] + '0');
-	}
-}
-
-/**
  * FORMAT_SPECIFIER - Macro to handle format specifiers through switch cases.
  */
 
@@ -71,7 +41,7 @@ void print_binary(uint64_t num)
 		count++;  \
 		break; \
 	case 'b': \
-		print_binary(va_arg(args, uint64_t)); \
+		print_binary(va_arg(args, int)); \
 		count++; \
 		break; \
 	default: \
