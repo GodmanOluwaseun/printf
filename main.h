5c67b872dd18ba6ea08da77e028d311f6991228f
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2023-11-20 03:12:53 -0800

    Printf function that hanldes %b format

--- main.h
+++ main.h
@@ -6,8 +6,41 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <stdint.h>
 
 int _printf(const char *format, ...);
+void print_binary(uint64_t num);
+
+
+/**
+ * print_binary - Converts ints to binary and prints.
+ *
+ * @num: Int to be converted.
+ * Return: nothing
+ */
+
+void print_binary(uint64_t num)
+{
+	int j, i = 0;
+	int binary[64];
+
+	if (num == 0)
+	{
+	putchar('0');
+	return;
+	}
+
+	while (num > 0)
+	{
+		binary[i++] = num % 2;
+		num /= 2;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		putchar(binary[j] + '0');
+	}
+}
 
 /**
  * FORMAT_SPECIFIER - Macro to handle format specifiers through switch cases.
